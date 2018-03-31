fd41bcee02c895e9635508823afb84a24342bb48
Afonso Raposo <afonsocraposo@gmail.com>
2018-03-31 23:38:32 +0100

    Grafos pesados (talvez acabado-ish)

--- src/Tipos_de_dados/WGraph.java
+++ src/Tipos_de_dados/WGraph.java
@@ -13,21 +13,43 @@ public class WGraph {
 			weight=w;
 			next=null;
 		}
+		
+		public Node get_to() {
+			return to;
+		}
+		
+		public int get_weight() {
+			return weight;
+		}
+		
 	}
 	
 	private class Children {
 		
 		private Edge last;
+		private int comp;
 		
 		private Children() {
 			last = null;
+			comp = 0;
 		}
 		
 		private void ins(Node n, int w) {
-			Edge e=new Edge(n, w);
+			Edge e = new Edge(n, w);
 			e.next=last;
 			last=e;
+			comp++;
 		}
+		
+		public Edge get_edge(int pos) {
+			Edge aux = last;
+			for(int i=comp-1;i>pos;i--) {
+				aux=last.next;
+			}
+			return aux;
+		}
+		
+		
 	}
 	
 	
@@ -42,21 +64,29 @@ public class WGraph {
 			next=null;
 			children = new Children();
 		}	
+		
+		public int val() {
+			return val;
+		}
+		
+		public Children child() {
+			return children;
+		}
 	}
 	
 	
 	private Node first;
+	private int comp;
 	
 	public WGraph(int n) {
 		
 		first=null;
+		comp=n;
 		
 		for(int i=0;i<n;i++) {
 			Node no = new Node(0);
-			if(i==0) {
-				no.next = first;
-				first=no;
-			}
+			no.next = first;
+			first=no;
 		}
 	}
 	
@@ -65,9 +95,28 @@ public class WGraph {
 		while(aux!=null) {
 			if(aux==n1) {
 				aux.children.ins(n2,w);
+				return;
 			}
 			aux=aux.next;
 		}
+	}
+	
+	public Node get_node(int pos) {
+		Node aux = first;
+		for(int i=comp-1;i>pos;i--) {
+			aux=first.next;
+		}
+		return aux;
+	}
+	
+	public static void main(String[] args) {
+		WGraph g = new WGraph(3);
+		Node n1 = g.get_node(1);
+		Node n2 = g.get_node(2);
+		g.add_edge(n1, n2, 3);
+		Edge e = n1.child().get_edge(0);
+		System.out.println(n2==e.get_to());
+		System.out.println(e.get_weight());
 	}
 	
 }
