83ab6c8c54d4b7c57a98d51d88d4233bf12af0e7
Lourenço A. Rodrigues <lourencomr97@gmail.com>
2018-05-04 14:26:09 +0100

    já se cria a amostra
    Alterei o Aprendizagem para ser mais organizado a ir buscar a informação que precisa, só no teach me! é que ele precisa de lêr de facto o ficheiro, e ao mesmo tempo precisa de saber que tipo de dados escolhemos para definir os domínios e os escloher do Domainoptions, que serão definidos corretamente logo que o professor forneça os dados

--- src/GUI/Aprendizagem.java
+++ src/GUI/Aprendizagem.java
@@ -3,14 +3,13 @@ package GUI;
 import java.awt.EventQueue;
 import java.awt.FileDialog;
 
-import Tipos_de_dados.Weighted_Graphs;
-
+import Tipos_de_dados.Amostra;
+import Tipos_de_dados.Sample;
 import javax.swing.JFrame;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.LinkedList;
 import java.io.IOException;
 
 import javax.swing.ButtonGroup;
@@ -27,6 +26,9 @@ public class Aprendizagem {
 
 	private JFrame frame;
 	private JTextField textField;
+	
+	private String selecteddatabase;
+	private String choosenparameter;
 
 	/**
 	 * Launch the application.
@@ -55,6 +57,10 @@ public class Aprendizagem {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		
+		
+		
 		frame = new JFrame();
 		frame.setBounds(300, 300, 700, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -67,91 +73,74 @@ public class Aprendizagem {
 				
 				FileDialog fd = new FileDialog(frame, "Select a file", FileDialog.LOAD);
 				frame.getContentPane().setLayout(null);
-				
-//				btnChooseFile.setBounds(15, 42, 115, 29);
-//				frame.getContentPane().add(btnChooseFile);
+
 				fd.setDirectory("C:\\");
-				fd.setFile("*.txt");
+				fd.setFile("*.csv");
 				fd.setVisible(true);
 				String filename = fd.getFile();
 				if (filename == null)
 					System.out.println("Cancelled");
 				else 
 					System.out.println("File selected: " + filename);
-				
-				File FILENAME = new File(fd.getDirectory()+filename);
-
-				BufferedReader br = null;
-				FileReader fr = null;
-				LinkedList<String> la = new LinkedList<String>();
-
-				try {
-
-					//br = new BufferedReader(new FileReader(FILENAME));
-					fr = new FileReader(FILENAME);
-					br = new BufferedReader(fr);
-
-					String sCurrentLine;
-
-					while ((sCurrentLine = br.readLine()) != null) {
-						la.add(sCurrentLine);
-					}
-
-				} catch (IOException a) {
-
-					a.printStackTrace();
-
-				} finally {
-
-					try {
-
-						if (br != null)
-							br.close();
-
-						if (fr != null)
-							fr.close();
-						
-						System.out.println(la);
-
-					} catch (IOException ex) {
-
-						ex.printStackTrace();
-
-					}
-				}
+				selecteddatabase=fd.getDirectory()+filename;
 				textField.setText(filename);
 			}
 		});
-		btnChooseFile.setBounds(15, 42, 115, 29);
+		btnChooseFile.setBounds(15, 40, 115, 30);
 		frame.getContentPane().add(btnChooseFile);
 		
 		textField = new JTextField();
-		textField.setBounds(145, 43, 268, 26);
+		textField.setBounds(145, 40, 270, 30);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblChooseParameter = new JLabel("Choose parameter");
-		lblChooseParameter.setBounds(478, 46, 136, 20);
+		lblChooseParameter.setBounds(480, 40, 140, 20);
 		frame.getContentPane().add(lblChooseParameter);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Breast Cancer");
-		rdbtnNewRadioButton.setBounds(488, 84, 155, 29);
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choosenparameter="Breast Cancer";
+			}
+		});
+		rdbtnNewRadioButton.setBounds(490, 85, 155, 30);
 		frame.getContentPane().add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Diabetes");
-		rdbtnNewRadioButton_1.setBounds(488, 134, 155, 29);
+		rdbtnNewRadioButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choosenparameter="Diabetes";
+			}
+		});
+		rdbtnNewRadioButton_1.setBounds(490, 135, 155, 30);
 		frame.getContentPane().add(rdbtnNewRadioButton_1);
 		
 		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Hepatitis");
-		rdbtnNewRadioButton_2.setBounds(488, 187, 155, 29);
+		rdbtnNewRadioButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choosenparameter="Hepatitis";
+			}
+		});
+		rdbtnNewRadioButton_2.setBounds(490, 185, 155, 30);
 		frame.getContentPane().add(rdbtnNewRadioButton_2);
 		
 		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Parkisons");
-		rdbtnNewRadioButton_3.setBounds(488, 238, 155, 29);
+		rdbtnNewRadioButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choosenparameter="Parkinsons";
+			}
+		});
+		rdbtnNewRadioButton_3.setBounds(490, 235, 155, 30);
 		frame.getContentPane().add(rdbtnNewRadioButton_3);
 		
 		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("Thyroid");
-		rdbtnNewRadioButton_4.setBounds(488, 290, 155, 29);
+		rdbtnNewRadioButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				choosenparameter="Thyroid";
+			}
+		});
+		rdbtnNewRadioButton_4.setBounds(490, 285, 155, 30);
 		frame.getContentPane().add(rdbtnNewRadioButton_4);
 		
 		ButtonGroup group = new ButtonGroup(); 
@@ -164,13 +153,59 @@ public class Aprendizagem {
 		JButton btnTeachMe = new JButton("Teach me!");
 		btnTeachMe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				System.out.println(selecteddatabase);//só para ver que ele trás até aqui
+				System.out.println(choosenparameter);//a base de dados e o tipo da mesma
+				
+				int[][] Domainoptions= {{10,10,10,10,2},{5,5,5,5,4},{1,2,3,4,5},{5,4,3,2,1},{2,4,6,8,10}};
+				int[] Domains=null;
+				switch(choosenparameter) {
+				case("Breast Cancer"):{ Domains= Domainoptions[0];
+				break;}
+				case("Diabetes"): { Domains= Domainoptions[1];
+				break;}
+				case("Hepatitis"): { Domains= Domainoptions[2];
+				break;}
+				case("Parkinsons"): { Domains= Domainoptions[3];
+				break;}
+				case("Thyroid"): { Domains= Domainoptions[4];
+				break;}
+				}
+
+				Sample amostra=new Amostra(Domains);
+				
+				try {
+					FileReader fr=new FileReader(selecteddatabase);
+					BufferedReader br=new BufferedReader(fr);
+					
+					String CurrentLine;
+					String[] line;
+					int[] dataentry=new int[Domains.length];
+					while((CurrentLine=br.readLine())!=null) {
+						line=CurrentLine.split(",");
+						for(int pos=0;pos<line.length;pos++) {
+							dataentry[pos]=Integer.parseInt(line[pos]);
+						}
+						amostra.add(dataentry);
+					}
+					br.close();
+					fr.close();
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+//Neste ponto a amostra já está criada, basta depois construir o grafo pesaqdo, aplicar-lhe MST e do resultado fazer a rede de Bayes.
+//Falta também ter a certeza de que forma estão escritos os ficheiros, mas tenho quase a certeza que serão .csv, pelo que fiz aqui foi pensando assim
 				
 //      CENAS FIXES				
 //  	Pega na amostra cria 
 				
 			}
 		});
-		btnTeachMe.setBounds(15, 327, 115, 29);
+		btnTeachMe.setBounds(15, 335, 115, 30);
 		frame.getContentPane().add(btnTeachMe);
 	}
 }
