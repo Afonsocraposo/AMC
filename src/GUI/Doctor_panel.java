0d1b7d435b07a26609ead38be0c0f99d1a45bdce
Afonso Raposo <afonsocraposo@gmail.com>
2018-05-14 14:47:17 +0100

    Seleção do aniversário a funcionar

--- src/GUI/Doctor_panel.java
+++ src/GUI/Doctor_panel.java
@@ -8,7 +8,6 @@ import javax.swing.JLabel;
 import java.awt.Insets;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
-import net.miginfocom.swing.MigLayout;
 import javax.swing.SwingConstants;
 
 import com.itextpdf.text.DocumentException;
@@ -22,8 +21,10 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Doctor_panel extends JPanel {
 	
@@ -34,6 +35,7 @@ public class Doctor_panel extends JPanel {
 	 * Create the panel.
 	 */
 	public Doctor_panel(String username) {
+		setBackground(Color.WHITE);
 		setSize(1000,120);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{32, 117, 63, 52, 171, 48, 39, 151, 246, 0, 0};
@@ -42,7 +44,7 @@ public class Doctor_panel extends JPanel {
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
-		JLabel lblNewLabel = new JLabel("Patient Name:");
+		JLabel lblNewLabel = new JLabel("Patient's Name:");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
@@ -51,6 +53,7 @@ public class Doctor_panel extends JPanel {
 		add(lblNewLabel, gbc_lblNewLabel);
 		
 		textName = new JTextField();
+		String default_val = textName.getText();
 		GridBagConstraints gbc_textName = new GridBagConstraints();
 		gbc_textName.anchor = GridBagConstraints.NORTH;
 		gbc_textName.fill = GridBagConstraints.HORIZONTAL;
@@ -105,7 +108,23 @@ public class Doctor_panel extends JPanel {
 		gbc_rdbtnF.gridy = 2;
 		add(rdbtnF, gbc_rdbtnF);
 		
-		JComboBox comboBoxYear = new JComboBox();
+		ArrayList<String> years_tmp = new ArrayList<String>();
+		for(int years = Calendar.getInstance().get(Calendar.YEAR); years>=1900; years--) {
+		    years_tmp.add(years+"");
+		}
+		
+		ArrayList<String> months_tmp = new ArrayList<String>();
+		for(int months = 1; months<=12; months++) {
+		    months_tmp.add(months+"");
+		}
+		
+		ArrayList<String> days_tmp = new ArrayList<String>();
+		for(int days = 1; days<=31; days++) {
+		    days_tmp.add(days+"");
+		}
+		
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		JComboBox comboBoxYear = new JComboBox(years_tmp.toArray());
 		GridBagConstraints gbc_comboBoxYear = new GridBagConstraints();
 		gbc_comboBoxYear.anchor = GridBagConstraints.NORTH;
 		gbc_comboBoxYear.fill = GridBagConstraints.HORIZONTAL;
@@ -113,8 +132,9 @@ public class Doctor_panel extends JPanel {
 		gbc_comboBoxYear.gridx = 1;
 		gbc_comboBoxYear.gridy = 3;
 		add(comboBoxYear, gbc_comboBoxYear);
-		
-		JComboBox comboBoxMonth = new JComboBox();
+	
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		JComboBox comboBoxMonth = new JComboBox(months_tmp.toArray());
 		GridBagConstraints gbc_comboBoxMonth = new GridBagConstraints();
 		gbc_comboBoxMonth.anchor = GridBagConstraints.NORTH;
 		gbc_comboBoxMonth.fill = GridBagConstraints.HORIZONTAL;
@@ -122,8 +142,9 @@ public class Doctor_panel extends JPanel {
 		gbc_comboBoxMonth.gridx = 2;
 		gbc_comboBoxMonth.gridy = 3;
 		add(comboBoxMonth, gbc_comboBoxMonth);
-		
-		JComboBox comboBoxDay = new JComboBox();
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		JComboBox comboBoxDay = new JComboBox(days_tmp.toArray());
 		GridBagConstraints gbc_comboBoxDay = new GridBagConstraints();
 		gbc_comboBoxDay.anchor = GridBagConstraints.NORTHWEST;
 		gbc_comboBoxDay.insets = new Insets(0, 0, 0, 5);
@@ -145,9 +166,16 @@ public class Doctor_panel extends JPanel {
 				}
 				
 	        	PatientInfo patient = new PatientInfo();
-	        	patient.name = textName.getText();
+
+	        	if(!textName.getText().isEmpty()) {
+	        		patient.name = textName.getText();
+	        	}
 	        	patient.gender = gender;
 	        	patient.doctor = username;
+	        	
+	        	patient.birth_year = Integer.parseInt(comboBoxYear.getSelectedItem().toString());
+	        	patient.birth_month = Integer.parseInt(comboBoxMonth.getSelectedItem().toString());
+	        	patient.birth_day = Integer.parseInt(comboBoxDay.getSelectedItem().toString());
 	        	
 	        	String DEST = "results_pdf/Report-" + timeStampFile + ".pdf";
 	            File file = new File(DEST);
