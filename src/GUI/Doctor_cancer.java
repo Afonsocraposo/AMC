f30b9ae81b8def29d3e22ec91876460740634ebb
Afonso Raposo <afonsocraposo@gmail.com>
2018-05-17 15:18:06 +0100

    Cancro e diabetes bonitos

--- src/GUI/Doctor_cancer.java
+++ src/GUI/Doctor_cancer.java
@@ -61,7 +61,6 @@ public class Doctor_cancer extends JPanel {
 		Jlabel_2.setVisible(false);
 		
 		textField = new JTextField();
-		
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		textField.setBounds(15, 345, 222, 26);
 		textField.setText(Arrays.toString(Arrays.copyOf(parameters, 10)));
@@ -74,19 +73,19 @@ public class Doctor_cancer extends JPanel {
 		JComboBox comboBox = new JComboBox();
 		comboBox.addItem(0);
 		comboBox.addItem(1);
-		comboBox.setBounds(135, 63, 60, 25);
+		comboBox.setBounds(101, 69, 60, 25);
 		add(comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.addItem(0);
-		comboBox_1.setBounds(135, 113, 60, 25);
+		comboBox_1.setBounds(101, 119, 60, 25);
 		add(comboBox_1);
 		
 		JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.addItem(0);
 		comboBox_2.addItem(1);
 		comboBox_2.addItem(2);
-		comboBox_2.setBounds(135, 163, 60, 25);
+		comboBox_2.setBounds(101, 169, 60, 25);
 		add(comboBox_2);
 		
 		JComboBox comboBox_3 = new JComboBox();
@@ -94,45 +93,45 @@ public class Doctor_cancer extends JPanel {
 		comboBox_3.addItem(1);
 		comboBox_3.addItem(2);
 		comboBox_3.addItem(3);
-		comboBox_3.setBounds(135, 213, 60, 25);
+		comboBox_3.setBounds(101, 219, 60, 25);
 		add(comboBox_3);
 		
 		JComboBox comboBox_4 = new JComboBox();
 		comboBox_4.addItem(0);
 		comboBox_4.addItem(1);
 		comboBox_4.addItem(2);
-		comboBox_4.setBounds(135, 263, 60, 25);
+		comboBox_4.setBounds(101, 269, 60, 25);
 		add(comboBox_4);
 		
 		JComboBox comboBox_5 = new JComboBox();
 		comboBox_5.addItem(0);
 		comboBox_5.addItem(1);
-		comboBox_5.setBounds(340, 63, 60, 25);
+		comboBox_5.setBounds(262, 69, 60, 25);
 		add(comboBox_5);
 		
 		JComboBox comboBox_6 = new JComboBox();
 		comboBox_6.addItem(0);
 		comboBox_6.addItem(1);
-		comboBox_6.setBounds(340, 113, 60, 25);
+		comboBox_6.setBounds(262, 119, 60, 25);
 		add(comboBox_6);
 		
 		JComboBox comboBox_7 = new JComboBox();
 		comboBox_7.addItem(0);
 		comboBox_7.addItem(1);
 		comboBox_7.addItem(2);
-		comboBox_7.setBounds(340, 163, 60, 25);
+		comboBox_7.setBounds(262, 169, 60, 25);
 		add(comboBox_7);
 		
 		JComboBox comboBox_8 = new JComboBox();
 		comboBox_8.addItem(0);
 		comboBox_8.addItem(1);
-		comboBox_8.setBounds(340, 213, 60, 25);
+		comboBox_8.setBounds(262, 219, 60, 25);
 		add(comboBox_8);
 		
 		JComboBox comboBox_9 = new JComboBox();
 		comboBox_9.addItem(0);
 		comboBox_9.addItem(1);
-		comboBox_9.setBounds(340, 263, 60, 25);
+		comboBox_9.setBounds(262, 269, 60, 25);
 		add(comboBox_9);
 		textField.addFocusListener(new FocusAdapter() {
 			@Override
@@ -428,54 +427,54 @@ public class Doctor_cancer extends JPanel {
 		});
 		
 		JLabel lblParmetro = new JLabel("Parameter 1");
-		lblParmetro.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblParmetro.setBounds(15, 68, 105, 20);
+		lblParmetro.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblParmetro.setBounds(15, 69, 105, 20);
 		add(lblParmetro);
 		
 		JLabel label = new JLabel("Parameter 2");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label.setBounds(15, 118, 105, 20);
+		label.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label.setBounds(15, 119, 105, 20);
 		add(label);
 		
 		
 		JLabel label_2 = new JLabel("Parameter 3");
-		label_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_2.setBounds(15, 168, 105, 20);
+		label_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_2.setBounds(15, 169, 105, 20);
 		add(label_2);
 		
 		JLabel label_3 = new JLabel("Parameter 4");
-		label_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_3.setBounds(15, 218, 105, 20);
+		label_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_3.setBounds(15, 219, 105, 20);
 		add(label_3);
 		
 		JLabel label_4 = new JLabel("Parameter 5");
-		label_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_4.setBounds(15, 268, 105, 20);
+		label_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_4.setBounds(15, 269, 105, 20);
 		add(label_4);
 		
 		JLabel label_1 = new JLabel("Parameter 6");
-		label_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_1.setBounds(222, 68, 105, 20);
+		label_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_1.setBounds(173, 69, 105, 20);
 		add(label_1);
 		
 		JLabel label_5 = new JLabel("Parameter 7");
-		label_5.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_5.setBounds(222, 118, 105, 20);
+		label_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_5.setBounds(173, 119, 105, 20);
 		add(label_5);
 		
 		JLabel label_6 = new JLabel("Parameter 8");
-		label_6.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_6.setBounds(222, 168, 105, 20);
+		label_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_6.setBounds(173, 169, 105, 20);
 		add(label_6);
 		
 		JLabel label_7 = new JLabel("Parameter 9");
-		label_7.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_7.setBounds(222, 218, 105, 20);
+		label_7.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_7.setBounds(173, 219, 105, 20);
 		add(label_7);
 		
 		JLabel label_8 = new JLabel("Parameter 10");
-		label_8.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		label_8.setBounds(222, 268, 105, 20);
+		label_8.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_8.setBounds(173, 269, 105, 20);
 		add(label_8);
 		
 		JLabel lblSelectValuesFor1 = new JLabel("Select values for each parameter:");
